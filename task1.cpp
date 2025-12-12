fe0f5bbabff3f7dce37e9f71abb8af17ef80384c
adnanali55 <f2025-0968@bnu.edu.pk>
2025-12-12 15:26:57 +0500

    Implement basic input/output for hours array

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int hours[6];
+    for (int i=0; i<6; i++) {
+        cin>>hours[i];
+    }
+    for (int i=0; i<6; i++) {
+        cout<<hours[i]<<" ";
+    }
+}
